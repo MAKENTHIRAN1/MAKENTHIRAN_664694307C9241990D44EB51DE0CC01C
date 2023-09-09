f61ddb500ffeeba98b059bff7a2c1ef26bd720c3
MAKENTHIRAN1 <144515732+MAKENTHIRAN1@users.noreply.github.com>
2023-09-08 23:37:55 -0700

    Add files via upload

---
+++ MAHI CHALLANGE UNIT 1/ch1.1.py
@@ -0,0 +1,15 @@
+#python program to find the factorialof a number provide by the user.
+#enter the value for a different result
+num=7
+#To take input from the user
+#num=int(input("Enter a number"))
+factorial = 1
+#check if the number is negative, positive or zero
+if num < 0:
+  print("Sorry, factorial does noy exist for negative numbers")
+elif num == 0:
+  print("The factorial of 0 is 1")
+else:
+   for i in range(1,num + 1):
+      factorial = factorial*i
+   print("The factorial of",num,"is",factorial)
